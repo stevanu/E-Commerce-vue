cea61bec967324cbcce1b0c01e664ca0f9d322c9
stevanu <ivanstevanu@gmail.com>
2025-06-25 23:23:35 +0700

    Update Offers

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <Hero />
     <Products />
     <categories />
+    <Offers />
   </div>
 </template>
 <script setup>
@@ -11,4 +12,5 @@ import Header from './components/Header.vue';
 import Hero from './components/HeroSection.vue';
 import Products from './components/Products.vue';
 import Categories from './components/Categories.vue';
+import Offers from './components/Offers.vue';
 </script>
